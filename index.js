0a8e8115fafc4356a4baad8c8ea607543d81fc1e
Geno M <giantpowpow2@gmail.com>
2019-08-09 05:20:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ function takeANumber(line, name) {
 // console.log(takeANumber(['John', 'Montrezl'], 'Billy'))
 
 let pokemon = ['Cyndaquil', 'Bulbasaur', 'Charmander', 'Squirtle'];
-<<<<<<< HEAD
+
 // let pokemon = [];
-=======
 // ashKetchum = [];
->>>>>>> 0a89daa0da795279699bfdff4e259f9abc39f1ae
 
 function nowServing(line) {
   if (line[0] === undefined) {
@@ -23,10 +21,8 @@ function nowServing(line) {
 // console.log(nowServing(pokemon));
 // console.log(pokemon);
 
-<<<<<<< HEAD
 function newPokemonArray(line) {
   for (let i = 0; i < line.length; i++) {
-=======
 /* var testArray = ['Cat', 'Dog'];
 
 function currentLine(line) {
@@ -41,7 +37,6 @@ console.log(currentLine(testArray));
 
 function newPokemonArray(line) {
   for (let i = 0; i < line.length; i++) {  
->>>>>>> 0a89daa0da795279699bfdff4e259f9abc39f1ae
     line[i] = `${line.indexOf(line[i])+1}. ${line[i]}`;
   };
   return line;
@@ -56,8 +51,4 @@ function currentLine(line) {
   };
 };
 
-<<<<<<< HEAD
 console.log(currentLine(pokemon));
-=======
-console.log(currentLine(pokemon));
->>>>>>> 0a89daa0da795279699bfdff4e259f9abc39f1ae
