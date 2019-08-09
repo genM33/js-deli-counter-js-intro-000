af9fc471b7deb9cb472767a598ff375bc47f5b71
Geno M <giantpowpow2@gmail.com>
2019-08-09 05:17:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ function takeANumber(line, name) {
 // console.log(takeANumber(['John', 'Montrezl'], 'Billy'))
 
 let pokemon = ['Cyndaquil', 'Bulbasaur', 'Charmander', 'Squirtle'];
+<<<<<<< HEAD
 // let pokemon = [];
+=======
+// ashKetchum = [];
+>>>>>>> 0a89daa0da795279699bfdff4e259f9abc39f1ae
 
 function nowServing(line) {
   if (line[0] === undefined) {
@@ -19,8 +23,25 @@ function nowServing(line) {
 // console.log(nowServing(pokemon));
 // console.log(pokemon);
 
+<<<<<<< HEAD
 function newPokemonArray(line) {
   for (let i = 0; i < line.length; i++) {
+=======
+/* var testArray = ['Cat', 'Dog'];
+
+function currentLine(line) {
+  line.forEach(arrayFunction);
+  function arrayFunction(item, index) {
+    return `${index}. ${item}`;
+  };
+};
+
+console.log(currentLine(testArray));
+*/
+
+function newPokemonArray(line) {
+  for (let i = 0; i < line.length; i++) {  
+>>>>>>> 0a89daa0da795279699bfdff4e259f9abc39f1ae
     line[i] = `${line.indexOf(line[i])+1}. ${line[i]}`;
   };
   return line;
@@ -35,4 +56,8 @@ function currentLine(line) {
   };
 };
 
+<<<<<<< HEAD
 console.log(currentLine(pokemon));
+=======
+console.log(currentLine(pokemon));
+>>>>>>> 0a89daa0da795279699bfdff4e259f9abc39f1ae
